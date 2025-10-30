ec0041da341c60b2fb3c7fb6b3ab9199e43145a9
XiaErr <42758930+XiaErr@users.noreply.github.com>
2025-10-30 11:52:17 -0400

    Update dailymotion_m3ugrabber.py

--- scripts/dailymotion_m3ugrabber.py
+++ scripts/dailymotion_m3ugrabber.py
@@ -30,7 +30,7 @@ def grab(line):
         response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['auto'][0]['url']
         m3u = s.get(response, proxies=proxies).text
         m3u = m3u.strip().split('\n')[1:]
-        d = {}
+        d = {1280x720}
         cnd = True
         for item in m3u:
             if cnd:
@@ -41,7 +41,7 @@ def grab(line):
                 d[resolution]= item
             cnd = not cnd
         #print(m3u)
-        m3u = d[720(d, key=int)]    
+        m3u = d[max(d, key=int)]    
     except Exception as e:
         m3u = na
     finally:
