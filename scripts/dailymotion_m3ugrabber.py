7535c100d4c09509bffccd373e4df1c433ffb6de
XiaErr <42758930+XiaErr@users.noreply.github.com>
2025-10-30 15:12:17 -0400

    Update dailymotion_m3ugrabber.py

--- scripts/dailymotion_m3ugrabber.py
+++ scripts/dailymotion_m3ugrabber.py
@@ -27,14 +27,14 @@ na = 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/mo
 def grab(line):
     try:
         _id = line.split('/')[4]
-        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['max'][0]['url']
+        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['auto'][0]['url']
         m3u = s.get(response, proxies=proxies).text
         m3u = m3u.strip().split('\n')[1:]
         d = {}
         cnd = True
         for item in m3u:
             if cnd:
-                resolution = item.strip().split(',')[2].split('=')[1]
+                resolution = item.strip().split(',')[2].split('=')[3]
                 if resolution not in d:
                     d[resolution] = []
             else:
